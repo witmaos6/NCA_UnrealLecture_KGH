68bb76787d4e841a52be53dd9374a273c80de2b8
WindOn <geonhee518@naver.com>
2023-11-06 14:08:15 +0900

    Init

---
+++ Source/NCAUnrealLectureKGH/NCAUnrealLectureKGH.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "NCAUnrealLectureKGH.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NCAUnrealLectureKGH, "NCAUnrealLectureKGH" );
